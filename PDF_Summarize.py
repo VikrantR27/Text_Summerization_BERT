1760148bf5122c65472d2565a19a775cdfa21a5d
Vikrant Rangnekar <vikrant.rangnekar@gmail.comgit config --global user.email vikrant.rangnekar@gmail.com>
2020-04-28 12:02:06 +0530

    Added Image text reader

--- PDF_Summarize.py
+++ PDF_Summarize.py
@@ -3,6 +3,8 @@ from summarizer import Summarizer
 import PyPDF2 
 import textract
 from docx import Document 
+import pytesseract
+from PIL import Image
 
 def PDFRead(filepath): 
     pdfFileObj = open(filepath, 'rb') 
@@ -23,14 +25,25 @@ def DOCRead(filepath):
     
     return(file)
 
+def IMGRead(filepath):
+    img=Image.open(filepath)
+    text=pytesseract.image_to_string(img)
+    return (text)
 
-text1 = "YOUR_FILE_PATH"
+text1 = "1.png"
 
 if text1[-3]+text1[-2]+text1[-1]=="pdf" :
     text=PDFRead(text1)
 
 if text1[-4]+text1[-3]+text1[-2]+text1[-1]=="docx" :
     text=DOCRead(text1)
+
+if text1[-3]+text1[-2]+text1[-1]=="png" :
+    text=IMGRead(text1)
+
+if text1[-4]+text1[-3]+text1[-2]+text1[-1]=="jpeg" :
+    text=IMGRead(text1)
+
     
 
 print(text)
